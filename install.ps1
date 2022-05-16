4532cb045fdf541521b0cb44f54f6bb264345029
Adi Purnomo <adi.superb@gmail.com>
2022-05-16 19:37:57 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,14 +2,30 @@ Write-Host "Hello The AVOLife!"
 Read-Host "Press any key to continue.."
 
 Set-ExecutionPolicy Bypass -Scope Process
-
+<#
+.Install Chocolatey
+#>
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 choco -?
 
+<#
+.Aplikasi Google
+#>
 choco install googlechrome -y
-choco install anydesk
-choco install anydesk -y
 choco install google-drive-file-stream -y
+choco install drawio -y
+
+<#
+.Nice to Have
+#>
+choco install anydesk -y
 choco install zoom -y
 choco install winrar -y
+choco install vlc -y
+choco install spotify -y
+choco install whatsapp -y
+choco install microsoft-teams -y
+choco install powertoys -y
+
+Write-Host "IT Arena #AvoWebSolutions"
